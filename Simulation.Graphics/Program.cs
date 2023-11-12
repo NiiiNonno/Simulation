1713a3041ebb918e7867dbad79cc9bad5c086f23
JSACL <j.s.and.c.lover@gmail.com>
2023-11-12 19:30:29 +0900

    Rustとの諧調。

---
+++ Simulation.Graphics/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Nonno.Simulation.Graphics;
+public class Program
+{
+    public static int Main(string[] args)
+    {
+        var p = new Program();
+
+        Set(p, Activator.Instance);
+
+        return 0;
+    }
+
+    public static void Set(Program program, Activator activator)
+    {
+        activator.CallGPU();
+        activator.CallWindow();
+    }
+}
